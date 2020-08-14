0aebf9ebbef18ba12c9e4b58322ee359a17bdcd8
Jason Leonhard <leonhard.jason@gmail.com>
2020-08-14 13:32:22 -0700

    update name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use grepy::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,7 +14,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = grepy::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
